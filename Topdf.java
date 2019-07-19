a3355910c2367732999a27bd7c711944917fbddc
kylak <gustavberloty2@gmail.com>
2019-07-19 12:41:30 +0200

    array aboutit a 95%

--- Topdf.java
+++ Topdf.java
@@ -7,11 +7,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-// L'ArrayList ne gère pas les mot coupés. Et on ne s'est pas occupé non plus de l'index 0 de ces ArrayList.
+// A faire : 1. S'occuper du bug au niveau de l'index. 2. Ajouter tout le texte sur l'index 0 de l'ArrayList.
 
 public class Topdf {
-
-    static String nomina_sacra = "";
 
     public static void main(String[] args) throws ParserConfigurationException, SAXException, DocumentException, IOException {
 
@@ -46,7 +44,7 @@ public class Topdf {
             nominasacra(index, manuscrit);
 
             if (node.getNodeType() == Node.ELEMENT_NODE)
-            { // Je n'ai pas trouvé de cas où ça ne rentrer pas dans cette boucle.
+            { // Je n'ai pas trouvé de cas où ça ne rentrait pas dans cette boucle.
                 
                 Element eElement = (Element) node;
 
@@ -59,84 +57,60 @@ public class Topdf {
                 int prochain_numero_de_page = page + 1;
                 String prochaine_page = Integer.toString(prochain_numero_de_page) + "10101";
 
-
+                // "PROCHAIN MOT" --------------------------------------------------------------------
                 if(eElement.getAttribute("n").equals(prochain_mot)) {
+                    
                     if(PrendreLeMot) {
                         manuscrit += " ";
                     }
                     if(PrendreLeMot) {
                         manuscrit += eElement.getTextContent();
-                        texte.get(texte.size()-1).get(texte.get(texte.size()-1).size()-1).add(eElement.getTextContent());
+                        texte.get(texte.size()-1).get(texte.get(texte.size()-1).size()-1).add(eElement.getTextContent()); // On ajoute le mot sur la dernière ligne qu'on ait de la dernière page "écrite".
                     }
                     numero_de_mot++;
+                    
                 }
 
+                // "PROCHAINE LIGNE" -----------------------------------------------------------------
                 else if(eElement.getAttribute("n").equals(prochaine_ligne)) {
-                    if(eElement.getElementsByTagName("lb").getLength() == 1 ) { // Pourquoi avais-je mis ">= 1" au lieu de "== 1" ?
-                        NodeList parties = eElement.getChildNodes();
-                        if(PrendreLeMot) manuscrit += " ";
-                        for (int tmp = 0; tmp < parties.getLength(); tmp++) {
-                            Node partie = parties.item(tmp);
-                            if (PrendreLeMot && partie.getNodeType() == Node.ELEMENT_NODE) // Quand on arrive sur <lb/>
-                            {
-                                if(partie.getNodeName() == "lb") manuscrit += "\n";
-                                Element ePartie = (Element) partie;
-                                manuscrit += ePartie.getTextContent();
-                            }
-                            else if (PrendreLeMot && partie.getNodeType() == Node.TEXT_NODE) // Quand on arrive sur une des deux parties du mot séparé par <lb/>.
-                                manuscrit += partie.getTextContent();
-                        }
+                    
+                    if(eElement.getElementsByTagName("lb").getLength() == 1 ) {
+                         manuscrit = mot_coupe(eElement, PrendreLeMot, manuscrit, texte);
                     }
-                    else {
-                        if(PrendreLeMot) {
-                            manuscrit += "\n";
-                        }
-                        if(PrendreLeMot) {
-                            manuscrit += eElement.getTextContent();
-                            texte.get(texte.size()-1).add(new ArrayList<String>());
-                            texte.get(texte.size()-1).get(texte.get(texte.size()-1).size()-1).add(eElement.getTextContent());
-                        }
+                    else if(PrendreLeMot) {
+                        manuscrit += "\n";
+                        manuscrit += eElement.getTextContent();
+                        classe_et_prepare_une_ligne(texte);
+                        texte.get(texte.size()-1).get(texte.get(texte.size()-1).size()-1).add(eElement.getTextContent());
                     }
                     numero_de_ligne++;
                     numero_de_mot = 1;
-
+                    
                 }
 
+                // "PROCHAINE PAGE" ------------------------------------------------------------------
                 else if(eElement.getAttribute("n").equals(prochaine_page)) {
-                    if(eElement.getElementsByTagName("pb").getLength() == 1 ) { // Pourquoi avais-je mis ">= 1" au lieu de "== 1" ?
-                        //manuscrit += "mot coupé";
-                        NodeList parties = eElement.getChildNodes();
-                        if(PrendreLeMot) manuscrit += " ";
-                        for (int tmp = 0; tmp < parties.getLength(); tmp++) {
-                            Node partie = parties.item(tmp);
-                            if (PrendreLeMot && partie.getNodeType() == Node.ELEMENT_NODE) // Quand on arrive sur <lb/>
-                            {
-                                if(partie.getNodeName() == "pb") manuscrit += "\n\n";
-                                Element ePartie = (Element) partie;
-                                manuscrit += ePartie.getTextContent();
-                            }
-                            else if (PrendreLeMot && partie.getNodeType() == Node.TEXT_NODE) // Quand on arrive sur une des deux parties du mot séparé par <lb/>.
-                                manuscrit += partie.getTextContent();
-                        }
+                    
+                    if(eElement.getElementsByTagName("pb").getLength() == 1 ) {
+                        manuscrit = mot_coupe(eElement, PrendreLeMot, manuscrit, texte);
                     }
-                    else {
-                        if( page !=0 && PrendreLeMot) {
-                            manuscrit += "\n\n";
-                        }
-                        if(PrendreLeMot) {
-                            manuscrit += eElement.getTextContent();
-                            texte.add(new ArrayList<ArrayList<String>>());
-                            texte.get(texte.size()-1).add(new ArrayList<String>());
-                            texte.get(texte.size()-1).get(texte.get(texte.size()-1).size()-1).add(eElement.getTextContent());
-                        }
+                    else if(PrendreLeMot) {
+                        if( page !=0 ) manuscrit += "\n\n";
+                        manuscrit += eElement.getTextContent();
+                        classe_et_prepare_une_page(texte);
+                        texte.get(texte.size()-1).get(texte.get(texte.size()-1).size()-1).add(eElement.getTextContent());
                     }
                     page++;
                     numero_de_ligne = 1;
                     numero_de_mot = 1;
+                    
                 }
 
             }
         }
+        
+        // texte.add(0, /* tout le texte */);
+        // manuscrit += texte.get(1).get(0).get(0);
         CreerPDF pdf = new CreerPDF(manuscrit, "032");
         pdf.generer();
     }
@@ -160,6 +134,60 @@ public class Topdf {
 
     static boolean isTheWordToIgnore(Node given, boolean finalMode) {
         return (given.getParentNode().getNodeName() == "rdg" && ( (Element) given.getParentNode() ).getAttribute("type").equals("orig") && ( (Element) given.getParentNode().getPreviousSibling() ).getAttribute("type").equals("corr") && finalMode);
+    }
+    
+    static String mot_coupe (Element eElement, boolean PrendreLeMot, String manuscript, ArrayList<ArrayList<ArrayList<String>>> texte) {
+        NodeList parties = eElement.getChildNodes();
+        if(PrendreLeMot) manuscript += " ";
+        for (int a = 0; a < parties.getLength(); a++) {
+            Node partie = parties.item(a);
+            manuscript += partie.getTextContent(); // Pour les parties qui sont entre balises par exemple : <unclear> (voir le α de ιακωβ à la ligne 2 de la page 1).
+            if( a == 0 ) texte.get(texte.size()-1).get(texte.get(texte.size()-1).size()-1).add(partie.getTextContent());
+            else if (texte.get(texte.size()-1).get(texte.get(texte.size()-1).size()-1).size()-1 >= 0) {
+                texte.get(texte.size()-1).get(texte.get(texte.size()-1).size()-1).set(
+                        texte.get(texte.size()-1).get(texte.get(texte.size()-1).size()-1).size()-1, texte.get(texte.size()-1).get(texte.get(texte.size()-1).size()-1).get(texte.get(texte.size()-1).get(texte.get(texte.size()-1).size()-1).size()-1) + partie.getTextContent()
+                );
+            }
+            else texte.get(texte.size()-1).get(texte.get(texte.size()-1).size()-1).add(partie.getTextContent());
+            if (PrendreLeMot && partie.getNodeType() == Node.ELEMENT_NODE) // Quand on arrive à l'intersection d'une nouvelle ligne (<lb/>) ou page (<pb/>) ("ELEMENT_NODE").
+            {
+                if (partie.getNodeName() == "pb") {
+                    manuscript += "\n\n";
+                    classe_et_prepare_une_page(texte); // Seulement utile pour l'array texte
+                }
+                else if (partie.getNodeName() == "lb") {
+                    manuscript += "\n";
+                    classe_et_prepare_une_ligne(texte); // Seulement utile pour l'array texte
+                }
+            }
+        }
+        return manuscript;
+    }
+    
+    static void classe_et_prepare_une_ligne (ArrayList<ArrayList<ArrayList<String>>> texte) {
+        String toute_la_ligne = "";
+        for (int j = 0; j <  texte.get(texte.size()-1).get(texte.get(texte.size()-1).size()-1).size(); j++) {
+            toute_la_ligne += texte.get(texte.size()-1).get(texte.get(texte.size()-1).size()-1).get(j) + " ";
+        }
+        toute_la_ligne = toute_la_ligne.substring(0, toute_la_ligne.length());
+        toute_la_ligne += "\n";
+        texte.get(texte.size()-1).get(texte.get(texte.size()-1).size()-1).add(0, toute_la_ligne);
+        texte.get(texte.size()-1).add(new ArrayList<String>());
+    }
+    
+    static void classe_et_prepare_une_page (ArrayList<ArrayList<ArrayList<String>>> texte) {
+        ArrayList<String> tmp = new ArrayList<String>();
+        String toute_la_page = "\n";
+        for (int i = 0; i < texte.get(texte.size()-1).size()-1; i++) { // Pourquoi on a size()-1 à la fin ?
+            toute_la_page += texte.get(texte.size()-1).get(i).get(0);
+            for (int j = 1; j < texte.get(texte.size()-1).get(i).size()-1; j++) {
+                tmp.add(texte.get(texte.size()-1).get(i).get(j));
+            }
+        }
+        tmp.add(0, toute_la_page);
+        texte.get(texte.size()-1).add(0, tmp);
+        texte.add(new ArrayList<ArrayList<String>>());
+        texte.get(texte.size()-1).add(new ArrayList<String>());
     }
 
 }
