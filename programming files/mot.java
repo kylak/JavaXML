2e04986c95f57c7443de8f4e7798dbc97e20dee8
kylak <gustavberloty2@gmail.com>
2020-02-22 00:53:07 +0100

    Update mot.java

--- programming files/mot.java
+++ programming files/mot.java
@@ -1,1 +1,32 @@
-/* Source :Gustav Berloty (author of this file),Alan Bunning (transcripted data and greek font, http://greekcntr.org/),Charles Lang Freer (original manuscript),and the One who created that manuscript …This file is copyright © 2020 by Gustav Berloty released under the CC BY-NC-SA 4.0 licence :https://creativecommons.org/licenses/by-nc-sa/4.0/The greek font "KoineGreek" is copyright © 2019 by Alan Bunning released under theCreative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License(CC BY-NC-ND 4.0). */class mot {    String valeur;    // La variable numero donnera la position du mot dans le le NT : dans quel livre est-il, quel chapitre, quel verset, quel position dans le verset.    String numero; // Le type est String car on peut avoir a utiliser un "leading zero".    // Pouvoir signaler si le mot est coupé en 2 et si oui si il s'agit de la première partie du mot ou de la seconde.    /*     On pourra ajouter d'autres caractèristiques du mot, comme par exemple :     - son numéro Strong     - si c'est un verbe, un nom, un pronom, un adjectif, etc…     - sa conjugaison     // ajouter sa position dans le manuscrit grâce à l'ArrayList texte ?     Nous pourrons ainsi avoir un code couleur selon certaines de ses propriétés.     */        public mot(String m) {        valeur = m;    }        /*public mot(String n) {        numero = n;    }*/        public mot(String m, String n) {        valeur = m;        numero = n;    }}
+/* Licence :
+This file is copyright © 2020 by Gustav Berloty (creator of this file) released under the CC BY-NC-SA 4.0 licence :
+https://creativecommons.org/licenses/by-nc-sa/4.0/
+*/
+
+class mot {
+    String valeur;
+    // La variable numero donnera la position du mot dans le le NT : dans quel livre est-il, quel chapitre, quel verset, quel position dans le verset.
+    String numero; // Le type est String car on peut avoir a utiliser un "leading zero".
+    // Pouvoir signaler si le mot est coupé en 2 et si oui si il s'agit de la première partie du mot ou de la seconde.
+    /*
+     On pourra ajouter d'autres caractèristiques du mot, comme par exemple :
+     - son numéro Strong
+     - si c'est un verbe, un nom, un pronom, un adjectif, etc…
+     - sa conjugaison
+     // ajouter sa position dans le manuscrit grâce à l'ArrayList texte ?
+     Nous pourrons ainsi avoir un code couleur selon certaines de ses propriétés.
+     */
+    
+    public mot(String m) {
+        valeur = m;
+    }
+    
+    /*public mot(String n) {
+        numero = n;
+    }*/
+    
+    public mot(String m, String n) {
+        valeur = m;
+        numero = n;
+    }
+}
